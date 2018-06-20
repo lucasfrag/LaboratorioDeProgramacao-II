06e34b17ce99514e98fbfcb50701a091208a27ae
Lucas Fraga <ti.lucasfraga@gmail.com>
2018-06-20 15:13:51 -0300

    Trabalho III - Sistema de Monetização com Banco de Dados

---
+++ Trabalho III - Sistema de Monetização com Banco de Dados/src/br/com/senacrs/lab2/monetizacao/entidades/Usuario.java
@@ -0,0 +1,50 @@
+package br.com.senacrs.lab2.monetizacao.entidades;
+
+public class Usuario {
+	private long cpf;
+	private String nome;
+	private String login;
+	private String senha;
+	
+	public Usuario(long cpf, String nome, String endereco, String login, String senha) {
+		super();
+		this.cpf = cpf;
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+
+}
